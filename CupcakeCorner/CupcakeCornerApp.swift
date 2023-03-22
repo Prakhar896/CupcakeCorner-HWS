1165b09e1e937a267cf9113668b9c6738091fc34
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2023-03-22 20:02:12 +0800

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Prakhar Trivedi on 22/3/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
