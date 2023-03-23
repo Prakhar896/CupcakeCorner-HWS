f04d983c22e3b6cdf97dd5aae55d9a64be6ce465
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2023-03-23 23:02:20 +0800

    completed first screen

---
+++ CupcakeCorner/Models.swift
@@ -0,0 +1,26 @@
+//
+//  Models.swift
+//  CupcakeCorner
+//
+//  Created by Prakhar Trivedi on 23/3/23.
+//
+
+import SwiftUI
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    @Published var type = 0
+    @Published var quantity = 3
+    
+    @Published var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+}
